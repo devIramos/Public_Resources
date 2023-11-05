26734f8a5592daf4eac006b8bc3c3f1bdc0f8f48
Ivan Ramos <near.251@gmail.com>
2023-11-05 01:09:32 +0000

    Carga inicial sitio estático

---
+++ web_examples/script.js
@@ -0,0 +1,8 @@
+// Aquí puedes agregar cualquier interacción con JavaScript que sea necesaria
+document.getElementById('login').addEventListener('click', function() {
+    // Aquí iría la lógica para iniciar sesión
+});
+
+document.getElementById('get-ms365').addEventListener('click', function() {
+    // Aquí iría la lógica para obtener Microsoft 365
+});
